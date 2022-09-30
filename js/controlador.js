6838a4839c05dc2048aa1377118847e029e978ba
unknown <mgilma018@gmail.com>
2022-09-29 19:34:58 -0500

    taller2.1 netxflix

---
+++ js/controlador.js
@@ -0,0 +1,26 @@
+/*
+import { pintarTienda } from "./llenadoTienda.js";
+
+let producto
+pintarTienda()
+
+let contenedorTienda=document.getElementById("fila")
+contenedorTienda.addEventListener("click",function(event){
+
+
+    if (event.target.classList.contains("btn")){
+
+
+    }
+})
+
+
+let botom=document.getElementById('boton')
+botom.addEventListener("click",function(event){
+    console.log('producto')
+})
+
+*/
+
+
+
